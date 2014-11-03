662074c50839ab61a3d9b30a56bfd04fbce9d722
Jason <ruankai@ruankaideMacBook-Pro.local>
2014-11-03 21:53:03 +0800

    Initial Commit

---
+++ kid/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  kid
+//
+//  Created by Jason on 14/11/3.
+//  Copyright (c) 2014年 Jason. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
