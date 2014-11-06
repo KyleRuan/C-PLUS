00b04d17817df83cf0a7f0cfe5aee6c8abbc2ec9
KyleRuan <kobe8329@gmail.com>
2014-11-06 10:45:15 +0800

    4.3.5

---
+++ Kidd.cpp
@@ -0,0 +1,18 @@
+//
+//  Kidd.cpp
+//  kid
+//
+//  Created by Jason on 14/11/5.
+//  Copyright (c) 2014年 Jason. All rights reserved.
+//
+
+
+//3.7---2
+#include <iostream>
+#include <cstring>
+    int main(int argc, const char * argv[]) {
+        using namespace std;
+       char16_t K[]=u"dddd";
+        cout<<K;
+        
+    }
