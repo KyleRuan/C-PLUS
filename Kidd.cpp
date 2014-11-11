f873a1557e567265b569ebdc3123860036fa8faa
KyleRuan <kobe8329@gmail.com>
2014-11-11 16:35:06 +0800

    5.9.5

--- Kidd.cpp
+++ Kidd.cpp
@@ -4,15 +4,33 @@
 //
 //  Created by Jason on 14/11/5.
 //  Copyright (c) 2014年 Jason. All rights reserved.
-//
 
+//5.9.5
+#include<iostream>
+int main()
+{ using namespace std;
+    const char* months[12] = { "January", "February", "March", "April",
+        "May", "June", "July", "August","September", "October", "November", "December"};
 
-//3.7---2
-#include <iostream>
-#include <cstring>
-    int main(int argc, const char * argv[]) {
-        using namespace std;
-       char16_t K[]=u"dddd";
-        cout<<K;
+    int salesnumber[3][12], sum[3] = {0};
+    for (int j=0; j<=2; j++) {
+         cout << "the "<<j+1<<" year\n";
+        for (int i = 0; i < 12; i++)
+        {
+            cout << "Please input the " << *(months + i) << " sales numbers:"<< endl;
+            cin >> salesnumber[j][i];
+            cin.get();
+            sum[j] += salesnumber[j][i];
+        }
         
+            }
+    
+    for (x:sum) {
+        cout << "The total sales number of this year is " << x<< endl;
     }
+    
+
+    cout<<"the total sales number of three years:"<<sum[0]+sum[2]+sum[1];
+    
+}
+
